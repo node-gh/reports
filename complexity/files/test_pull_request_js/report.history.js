c0f6d0c60698ae3d66a433692c2a8c9a8a394ca1
gh octocat <octocat@nodegh.io>
2015-06-10 01:22:51 +0000

    Source code report for node-gh/gh@bf6e6f945e6da6e5ec436a4fd05622239472750c

---
+++ complexity/files/test_pull_request_js/report.history.js
@@ -0,0 +1,1 @@
+__history = [{"date":"Wed, 10 Jun 2015 01:22:43 GMT","sloc":58,"lloc":32,"functions":11,"deliveredBugs":0.3020519202981321,"maintainability":80.76258038732105,"lintErrors":[],"difficulty":10.054054054054054}]
